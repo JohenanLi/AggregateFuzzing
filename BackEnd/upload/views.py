6def3e70e0ae3f4f7d26aca7480ced9fc7323abe
JohenanLi <599192367@qq.com>
2021-03-09 21:33:05 +0800

    基本对接成功

--- BackEnd/upload/views.py
+++ BackEnd/upload/views.py
@@ -1,3 +1,4 @@
+from BackEnd.settings import BASE_DIR
 from FuzzAll.fuzz import fuzz_one
 from django.http import HttpResponse
 from django.views import generic
@@ -5,6 +6,7 @@ from django.shortcuts import render, HttpResponse, redirect
 from django.views.generic.base import TemplateView, View
 from .models import *
 import os
+import _thread
 # Create your views here.
 
 
@@ -12,49 +14,86 @@ class IndexView(generic.TemplateView):
     template_name = 'index.html'
 
 
+def threadFuzz(fuzzer, program_path, isqemu, ins, outs, params, isfile, codeOrProgramBoolean: bool, codeOrProgram):
+    result = fuzz_one(fuzzer=fuzzer, program_path=program_path,
+                      isqemu=True, ins=ins, outs=outs, params=params, isfile=isfile)
+    if codeOrProgramBoolean:
+        codeResult.create(codeCoverage=result['codeCoverage'],
+                          bugs=result['bugs'], sample=result['sample'], code=codeOrProgram)
+    else:
+        programResult.create(
+            codeCoverage=result['codeCoverage'], bugs=result['bugs'], sample=result['sample'], code=codeOrProgram)
+
+
 def sourceCode(request):
     if request.method == 'GET':
         seedList = ''
-        with open('seed.txt','r') as f:
+        with open('seed.txt', 'r') as f:
             seedList = f.readlines()
         f.close()
-        return render(request, 'sourceCode/sourceCode.html',{'seedList':seedList})
+        return render(request, 'sourceCode/sourceCode.html', {'seedList': seedList})
     elif request.method == 'POST':
         print(request.POST)
         filePath = request.FILES.get("myfile", None)
         name = request.POST['name']
         seed = request.POST['seed']
-        inputFile = request.FILES.get('inputFile',None)
+        inputFile = request.FILES.get('inputFile', None)
         parameter = request.POST['parameter']
         compileCommand = request.POST['compileCommand']
         inputCommand = request.POST['inputCommand']
-        fuzz_one()
+        outs = os.path.join(BASE_DIR, 'outs')
+
+        print('test')
         if not filePath:
             return HttpResponse("no files for upload!")
         else:
-            ifile = False
+            isfile = False
+            temp = uploadSourceCode.objects.create(
+                filePath=filePath, name=name, ins=seed, inputFile=inputFile, parameter=parameter, compileCommand=compileCommand, inputCommand=inputCommand)
             if not inputFile:
-                ifile = True
+                isfile = True
+                _thread.start_new_thread(threadFuzz(
+                    fuzzer=name, program_path=filePath, isqemu=True, ins=seed, outs=outs, params=parameter, isfile=isfile,codeOrProgramBoolean=True,codeOrProgram=temp))
             else:
                 # 调用接口传数据
-                uploadSourceCode.objects.create(
-                    filePath=filePath, name=name, parameter=parameter)
-                return render(request, 'sourceCode/wait.html')
+                _thread.start_new_thread(threadFuzz(
+                    fuzzer=name, program_path=filePath, isqemu=True, ins=seed, outs=outs, params=parameter, isfile=isfile,codeOrProgramBoolean=True,codeOrProgram=temp))
+
+            return render(request, 'sourceCode/wait.html', {'object': temp})
 
 
 def sourceProgram(request):
     if request.method == 'GET':
-        return render(request, 'sourceProgram/sourceProgram.html')
+        seedList = ''
+        with open('seed.txt', 'r') as f:
+            seedList = f.readlines()
+        f.close()
+        return render(request, 'sourceProgram/sourceProgram.html', {'seedList': seedList})
     elif request.method == 'POST':
         print(request.POST)
         filePath = request.FILES.get("myfile", None)
         name = request.POST['name']
+        seed = request.POST['seed']
+        inputFile = request.FILES.get('inputFile', None)
         parameter = request.POST['parameter']
         compileCommand = request.POST['compileCommand']
+        inputCommand = request.POST['inputCommand']
+        outs = os.path.join(BASE_DIR, 'outs')
+
+        print('test')
         if not filePath:
             return HttpResponse("no files for upload!")
         else:
-            # 调用接口传数据
-            uploadSourceProgram.objects.create(
-                filePath=filePath, name=name, parameter=parameter, compileCommand=compileCommand)
-            return render(request, 'sourceCode/wait.html')
+            isfile = False
+            temp = uploadSourceProgram.objects.create(
+                filePath=filePath, name=name, ins=seed, inputFile=inputFile, parameter=parameter, inputCommand=inputCommand)
+            if not inputFile:
+                isfile = True
+                _thread.start_new_thread(threadFuzz(
+                    fuzzer=name, program_path=filePath, isqemu=True, ins=seed, outs=outs, params=parameter, isfile=isfile,codeOrProgramBoolean=False,codeOrProgram=temp))
+            else:
+                # 调用接口传数据
+                _thread.start_new_thread(threadFuzz(
+                    fuzzer=name, program_path=filePath, isqemu=True, ins=seed, outs=outs, params=parameter, isfile=isfile,codeOrProgramBoolean=False,codeOrProgram=temp))
+
+                return render(request, 'sourceProgram/wait.html', {object: temp})
