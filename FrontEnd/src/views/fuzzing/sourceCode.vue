132d19a7394a2f81d2919188dbea477adb3a720b
JohenanLi <599192367@qq.com>
2021-03-28 20:45:03 +0800

    frontend

--- FrontEnd/src/views/fuzzing/sourceCode.vue
+++ FrontEnd/src/views/fuzzing/sourceCode.vue
@@ -1,85 +1,185 @@
 
 <template>
-<el-form ref="form" :model="form" label-width="80px">
+  <el-form ref="form" :model="form" label-width="auto" :label-position="right">
+    <el-form-item label="上传源代码">
+      <el-upload
+        class="sourceCode"
+        drag
+        action="https://jsonplaceholder.typicode.com/posts/"
+        :before-remove="beforeRemove"
+        multiple
+        :file-list="form.fileList"
+      >
+        <i class="el-icon-upload"></i>
+        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+      </el-upload>
+    </el-form-item>
+
+    <el-form-item label="fuzz软件">
+      <el-select v-model="form.name" placeholder="请选择使用的fuzz软件">
+        <el-option label="afl" value="afl"></el-option>
+        <el-option label="tortoise" value="tortoise"></el-option>
+        <el-option label="emem" value="emem"></el-option>
+      </el-select>
+    </el-form-item>
+
+    <el-form-item label="种子选取" size="small">
+      <div>
+        <el-checkbox-group v-model="form.seed">
+          <el-checkbox-button
+            v-for="city in cities"
+            :label="city"
+            :key="city"
+            >{{ city }}</el-checkbox-button
+          >
+        </el-checkbox-group>
+      </div>
+    </el-form-item>
+
+<el-form-item label="上传输入文件">
+  <div style="border 0px;">
 <el-upload
-  class="upload-demo"
+  class="inputFile"
   drag
   action="https://jsonplaceholder.typicode.com/posts/"
   :before-remove="beforeRemove"
-  multiple
-  :file-list="fileList"
->
-  
+  :file-list="form.fileList"
+  multiple>
   <i class="el-icon-upload"></i>
-<div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+  <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
 </el-upload>
-  <el-form-item label="活动区域">
-    <el-select v-model="form.region" placeholder="请选择活动区域">
-      <el-option label="区域一" value="shanghai"></el-option>
-      <el-option label="区域二" value="beijing"></el-option>
-    </el-select>
-  </el-form-item>
-  <el-form-item label="活动时间">
-    <el-col :span="11">
-      <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
-    </el-col>
-    <el-col class="line" :span="2">-</el-col>
-    <el-col :span="11">
-      <el-time-picker placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
-    </el-col>
-  </el-form-item>
-  <el-form-item label="即时配送">
-    <el-switch v-model="form.delivery"></el-switch>
-  </el-form-item>
-  <el-form-item label="活动性质">
-    <el-checkbox-group v-model="form.type">
-      <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-      <el-checkbox label="地推活动" name="type"></el-checkbox>
-      <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-      <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-    </el-checkbox-group>
-  </el-form-item>
-  <el-form-item label="特殊资源">
-    <el-radio-group v-model="form.resource">
-      <el-radio label="线上品牌商赞助"></el-radio>
-      <el-radio label="线下场地免费"></el-radio>
-    </el-radio-group>
-  </el-form-item>
-  <el-form-item label="活动形式">
-    <el-input type="textarea" v-model="form.desc"></el-input>
-  </el-form-item>
-  <el-form-item>
-    <el-button type="primary" @click="onSubmit">立即创建</el-button>
-    <el-button>取消</el-button>
-  </el-form-item>
-</el-form>
+</div>
+</el-form-item>
+
+<el-form-item label="时间选择">
+  <el-input-number v-model="form.time" :step="5"></el-input-number> 分钟
+</el-form-item>
+
+<el-form-item label="编译命令">
+<el-input
+  type="compileCommand"
+  :autosize="{ minRows: 2, maxRows: 4}"
+  placeholder="请输入内容"
+  v-model="form.compileCommand">
+</el-input>
+</el-form-item>
+
+<el-form-item label="输入命令">
+<el-input
+  type="inputCommand"
+  :autosize="{ minRows: 2, maxRows: 4}"
+  placeholder="请输入内容"
+  v-model="form.inputCommand">
+</el-input>
+</el-form-item>
+
+<el-form-item label="参数">
+<el-input
+  type="parameter"
+  :autosize="{ minRows: 2, maxRows: 4}"
+  placeholder="请输入内容"
+  v-model="form.parameter">
+</el-input>
+</el-form-item>
+
+
+
+    <el-form-item>
+      <el-button type="primary" @click="onSubmit">提交</el-button>
+      <el-button>取消</el-button>
+    </el-form-item>
+  </el-form>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        form: {
-           fileList: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}, {name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}]
-      ,
-          region: '',
-          date1: '',
-          date2: '',
-          delivery: false,
-          type: [],
-          resource: '',
-          desc: ''
-        }
-      }
+const cityOptions = [
+  "602",
+  "abw",
+  "bmi",
+  "cdr",
+  "cgm",
+  "cmx",
+  "dif",
+  "docx",
+  "dxf",
+  "eot",
+  "eps",
+  "fh",
+  "fodg",
+  "fodp",
+  "fods",
+  "fodt",
+  "html",
+  "hwp",
+  "key6",
+  "lwp",
+  "met",
+  "mml",
+  "mtp",
+  "ole",
+  "pcd",
+  "pct",
+  "pcx",
+  "pmd",
+  "ppm",
+  "ppt",
+  "pptx",
+  "psd",
+  "pub",
+  "qpw",
+  "qxp",
+  "ras",
+  "rtf",
+  "scrtf",
+  "sda",
+  "sdc",
+  "sdd",
+  "sdw",
+  "slk",
+  "svm",
+  "tga",
+  "tif",
+  "vdx",
+  "vsd",
+  "vsdx",
+  "wks",
+  "wmf",
+  "ww2",
+  "ww6",
+  "ww8",
+  "xbm",
+  "xls",
+  "xlsx",
+  "xpm",
+  "zip",
+  "zmf",
+];
+export default {
+  data() {
+    return {
+      cities: cityOptions,
+      form: {
+        fileList: [],
+        seed: [],
+        name: "",
+        inputFile: [],
+        compileCommand:"",
+        inputCommand:"",
+        parameter:"",
+        time:5
+      },
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log("submit!");
     },
-    methods: {
-      onSubmit() {
-        console.log('submit!');
-      }
-    }
-  }
+    beforeRemove(file) {
+      return this.$confirm(`确定移除 ${file.name}？`);
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
