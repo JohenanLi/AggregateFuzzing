0464861f56aa96d79f123e0ebe8d6380d9fb8077
JohenanLi <599192367@qq.com>
2021-04-22 12:43:21 +0800

    ok

--- FrontEnd/src/views/fuzzing/sourceCode.vue
+++ FrontEnd/src/views/fuzzing/sourceCode.vue
@@ -7,7 +7,7 @@
     :label-position="right"
     :rules="rules"
   >
-    <el-form-item label="软件名称" prop="programName" >
+    <el-form-item label="软件名称" prop="programName">
       <el-input v-model="form.programName" placeholder="请输入内容"></el-input>
     </el-form-item>
 
@@ -35,7 +35,7 @@
       </el-select>
     </el-form-item>
 
-    <el-form-item label="编译命令"  prop="compileCommand">
+    <el-form-item label="编译命令" prop="compileCommand">
       <!-- <el-input
         type="compileCommand"
         :autosize="{ minRows: 2, maxRows: 4 }"
@@ -84,7 +84,13 @@
     </el-form-item>
     <!--         :on-success="getCodePath"            :on-success="getInputFilePath" -->
     <el-form-item label="时间选择">
-      <el-input-number v-model="form.time" :step="1" :min='1' :max="24"></el-input-number> 小时
+      <el-input-number
+        v-model="form.time"
+        :step="1"
+        :min="1"
+        :max="24"
+      ></el-input-number>
+      小时
     </el-form-item>
 
     <el-form-item label="输入命令">
@@ -195,64 +201,58 @@ export default {
         parameter: "",
         time: 1,
       },
-      rules:{
+      rules: {
         programName: [
-            { required: true, message: '请输入软件名称', trigger: 'blur' },
-          ],
-        fileList:[
-            { required: true, message: '请上传文件', trigger: 'blur' },
-          ],
-        name: [
-            { required: true, message: '请选择活动名称', trigger: 'blur' },
-          ],
+          { required: true, message: "请输入软件名称", trigger: "blur" },
+        ],
+        fileList: [{ required: true, message: "请上传文件", trigger: "blur" }],
+        name: [{ required: true, message: "请选择活动名称", trigger: "blur" }],
         compileCommand: [
-            { required: true, message: '请选择编译命令', trigger: 'blur' },
-          ],
-      }
-
+          { required: true, message: "请选择编译命令", trigger: "blur" },
+        ],
+      },
     };
   },
   methods: {
     onSubmit() {
-       this.$refs['this.form'].validate((valid) => {
-          if (valid) {
-            //  var formData = new FormData()
-      let params = {
-        fileList: this.form.fileList,
-        seed: this.form.seed,
-        name: this.form.name,
-        inputFile: this.form.inputFile,
-        compileCommand: this.form.compileCommand,
-        inputCommand: this.form.inputCommand,
-        parameter: this.form.parameter,
-        time: this.form.time,
-        programName: this.form.programName,
-      };
-      // formData.append('seed', this.form.seed),
-      // formData.append('name',this.form.name),
-      // formData.append('compileCommand', this.form.compileCommand),
-      // formData.append('inputCommand', this.form.inputCommand),
-      // formData.append('paramete', this.form.parameter),
-      // formData.append('time',this.form.time),
-      // console.log(formData),
-      // axios({
-      //   url: "http://127.0.0.1:8000/upload/sourceCode/",
-      //   method: "post",
-      //   data: Qs.stringify(params)
-      // });
-      console.log(params);
-      formdataTest(params).then((res) => {
-        console.log(res);
-        if (res.data.status == 200) {
-          alert("success!");
+      this.$refs["this.form"].validate((valid) => {
+        if (valid) {
+          //  var formData = new FormData()
+          let params = {
+            fileList: this.form.fileList,
+            seed: this.form.seed,
+            name: this.form.name,
+            inputFile: this.form.inputFile,
+            compileCommand: this.form.compileCommand,
+            inputCommand: this.form.inputCommand,
+            parameter: this.form.parameter,
+            time: this.form.time,
+            programName: this.form.programName,
+          };
+          // formData.append('seed', this.form.seed),
+          // formData.append('name',this.form.name),
+          // formData.append('compileCommand', this.form.compileCommand),
+          // formData.append('inputCommand', this.form.inputCommand),
+          // formData.append('paramete', this.form.parameter),
+          // formData.append('time',this.form.time),
+          // console.log(formData),
+          // axios({
+          //   url: "http://127.0.0.1:8000/upload/sourceCode/",
+          //   method: "post",
+          //   data: Qs.stringify(params)
+          // });
+          console.log(params);
+          formdataTest(params).then((res) => {
+            console.log(res);
+            if (res.data.status == 200) {
+              alert("success!");
+            }
+          });
+        } else {
+          console.log("error submit!!");
+          return false;
         }
       });
-          } else {
-            console.log('error submit!!');
-            return false;
-          }
-        });
-     
     },
     beforeRemove(file) {
       return this.$confirm(`确定移除 ${file.name}？`);
